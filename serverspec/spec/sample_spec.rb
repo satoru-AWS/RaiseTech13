04281b4b1728605075a78ba499ab7b2d9c00fa41
satoru <tennis0878@yahoo.co.jp>
2025-06-13 22:06:04 +0900

    add ansible

--- serverspec/spec/sample_spec.rb
+++ serverspec/spec/sample_spec.rb
@@ -3,3 +3,24 @@ require 'spec_helper'
 describe package('nginx') do
     it { should be_installed }
 end
+
+describe file('/etc/nginx/conf.d/rails.conf') do
+    it { should exist }
+    it { should be_installed }
+end
+
+describe service('nginx') do
+    it { should be_running }
+end
+
+describe command('ruby -v') do
+    its(:stdout) { should match /ruby 3\.2\.3/ }
+end
+
+describe package('bundler') do
+    it { should be_installed.by('gem').with_version('2.3.14') }
+end
+
+describe port(listen_port) do
+    it { should be_listning }
+end
