c97ec660bada88c583309fcb3a7a462ab5d258a9
satoru <tennis0878@yahoo.co.jp>
2025-06-13 22:31:57 +0900

    add serverspec

--- serverspec/spec/sample_spec.rb
+++ serverspec/spec/sample_spec.rb
@@ -6,19 +6,19 @@ end
 
 describe file('/etc/nginx/conf.d/rails.conf') do
     it { should exist }
-    it { should be_installed }
+    it { should be_file }
 end
 
 describe service('nginx') do
     it { should be_running }
 end
 
-describe command('ruby -v') do
+describe command('bash -lc "ruby -v"') do
     its(:stdout) { should match /ruby 3\.2\.3/ }
 end
 
-describe package('bundler') do
-    it { should be_installed.by('gem').with_version('2.3.14') }
+describe command('bash -lc "bundler -v"') do
+    its(:stdout) { should match /Bundler version 2\.3\.14/ }
 end
 
 describe port(80) do
