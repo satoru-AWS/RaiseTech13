7234ff6b12b6b9ee2ace3c4198ccdcc93df95908
satoru <tennis0878@yahoo.co.jp>
2025-06-15 22:33:50 +0900

    add ansible

--- serverspec/spec/sample_spec.rb
+++ serverspec/spec/sample_spec.rb
@@ -17,10 +17,6 @@ describe command('/home/ec2-user/.rbenv/shims/ruby -v') do
     its(:stdout) { should match /ruby 3\.2\.3/ }
 end
 
-describe command('/home/ec2-user/.rbenv/shims/bundler -v') do
-    its(:stdout) { should match /Bundler version 2\.3\.14/ }
-end
-
 describe port(80) do
     it { should be_listening }
 end
